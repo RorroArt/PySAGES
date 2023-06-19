65226d46b733eb6cc088bcb6faf3f5731f719c9b
RorroArt <rorroarturo@uchicago.edu>
2023-06-19 17:12:34 -0500

    added iteration to lrs update function

--- pysages/ml/lr_schedule.py
+++ pysages/ml/lr_schedule.py
@@ -10,138 +10,132 @@ class LRSchedule:
     interation and a function that updates the learning rate for the next optimizer 
     operation. 
     """
-    last_epoch: int
-    learning_rate: int
+    learning_rate: Scalar
     update: Callable
     step_count: int = 0
 
 
 @dataclass
 class StepLR(LRSchedule):
-    def __init__(self, learning_rate, step_size, gamma, last_epoch=-1):
-        self.learning_rate = learning_rate
+    def __init__(self, learning_rate, step_size, gamma):
         self.step_size = step_size
         self.gamma = gamma
-        self.last_epoch = last_epoch
+        last_iter = last_epoch
 
         
-    def update(self):
+    def update(self, i):
         learning_rate = self.learning_rate
+        last_iter = i-1
 
-        if self.last_epoch == 0 or self.last_epoch % self.step_size == 0:
+        if last_iter == 0 or last_iter % self.step_size == 0:
             self.learning_rate = learning_rate * gamma
             return learning_rate * gamma 
 
-        self.last_epoch += 1           
         self.step_count += 1
 
         return learning_rate
 
 @dataclass 
 class MultiStepLR(LRSchedule):
-    def __init__(self, learning_rate, milestones, gamma, last_epoch=-1):
+    def __init__(self, learning_rate, milestones, gamma):
         self.learning_rate = learning_rate
         self.milestones = milestones
         self.gamma = gamma
-        self.last_epoch = last_epoch
 
-    def update(self):
+    def update(self, i):
         learning_rate = self.learning_rate
-        
-        if self.last_epoch == 0 or self.last_epoch in self.milestones:
+        last_iter = i - 1
+
+        if last_iter == 0 or last_iter in self.milestones:
             self.learning_rate = learning_rate * gamma
             return learning_rate * gamma 
         
         self.step_count += 1
-        self.last_epoch += 1
 
         return learning_rate
 
 @dataclass
 class CosineAnnealingLR(LRSchedule):
-    def __init__(self, T_max, eta_min=0, last_epoch=-1):
+    def __init__(self, T_max, eta_min=0):
         self.T_max = T_max
         self.eta_min = eta_min
-        self.last_epoch = last_epoch
-
-    def update(self):
+        
+    def update(self, i):
         learning_rate = self.learning_rate
+        last_iter = i-1
     
-        if self.last_epoch == 0:
+        if last_iter == 0:
             return learning_rate
-        elif self.step_count and self.last_epoch > 0:
+        elif self.step_count and last_iter > 0:
             learning_rate = self.eta_min + (learning_rate - self.eta_min) * (1-math.cos(math.pi / self.T_max)) / 2
-        elif (self.last_epoch - 1 - self.T_max) % (2 * self.T_max):
+        elif (last_iter - 1 - self.T_max) % (2 * self.T_max):
             learning_rate = + (base_lr - self.eta_min) * (1 - math.cos(math.pi / self.T_max)) / 2
         else:
-            learning_rate = (1 + math.cos(math.pi * self.last_epoch / self.T_max)) / (1 + math.cos(math.pi * (self.last_epoch - 1) / self.T_max)) * (learning_rate - self.eta_min) + self.eta_min
+            learning_rate = (1 + math.cos(math.pi * last_iter / self.T_max)) / (1 + math.cos(math.pi * (last_iter - 1) / self.T_max)) * (learning_rate - self.eta_min) + self.eta_min
         
         self.learning_rate = learning_rate
         self.step_count += 1
-        self.last_epoch += 1
 
         return learning_rate
 
 @dataclass
 class ConstantLR(LRSchedule):
-    def __init__(self, factor=1/3, max_iters=5, last_epoch=-1):
+    def __init__(self, factor=1/3, max_iters=5):
         self.factor = factor
         self.max_iters = max_iters
-        self.last_epoch = last_epoch
 
         assert factor <= 1 and factor >= 0, 'Factor must be between 0 and 1' 
 
-    def update(self):
+    def update(self, i):
         learning_rate = self.learning_rate
+        last_iter = i-1
     
-        if self.last_epoch == 0 or learning_rate < self.max_iters:
+        if last_iter == 0 or learning_rate < self.max_iters:
             learning_rate = learning_rate + self.factor
 
         self.learning_rate = learning_rate
         self.step_count += 1
-        self.last_epoch += 1
 
         return learning_rate
 
 @dataclass
 class ConstantLR(LRSchedule):
-    def __init__(self, factor=1/3, max_iters=5, last_epoch=-1):
+    def __init__(self, factor=1/3, max_iters=5):
         self.factor = factor
         self.max_iters = max_iters
-        self.last_epoch = last_epoch
+        last_iter = last_epoch
 
         assert factor <= 1 and factor >= 0, 'Factor must be between 0 and 1' 
 
-    def update(self):
+    def update(self, i):
         learning_rate = self.learning_rate
+        last_iter = i - 1
     
-        if self.last_epoch == 0 or learning_rate < self.max_iters:
+        if last_iter == 0 or learning_rate < self.max_iters:
             learning_rate = learning_rate * self.factor
 
         self.step_count += 1
-        self.last_epoch += 1
 
         return learning_rate
 
 @dataclass
 class LinearLR(LRSchedule):
-    def __init__(self, inital_factor=1/3, final_factor=1, max_iters=5, last_epoch=-1):
+    def __init__(self, inital_factor=1/3, final_factor=1, max_iters=5):
         self.initial_factor = initial_factor
         self.final_factor = final_factor
         self.max_iters = max_iters
-        self.last_epoch = last_epoch
 
         assert initial_factor <= 1 and factor >= 0, 'Initial factor must be between 0 and 1'
         assert final_factor <= 1 and factor >= 0, 'Final factor must be between 0 and 1'  
 
-    def update(self):
+    def update(self, i):
         learning_rate = self.learning_rate
+        last_iter = i - 1
     
-        if self.last_epoch == 0 or learning_rate < self.max_iters:
+        if last_iter == 0 or learning_rate < self.max_iters:
             learning_rate = learning_rate * self.factor
 
         self.learning_rate = learning_rate
         self.step_count += 1
-        self.last_epoch += 1
 
         return learning_rate
